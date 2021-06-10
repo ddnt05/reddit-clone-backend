16cfe1bf8800081e15ca897b6d5e177289c5dcf9
ddnt05 <kerekesd05@gmail.com>
2021-06-10 16:58:54 +0200

    models and repos created

---
+++ src/main/java/com/example/redditclone/model/Post.java
@@ -0,0 +1,32 @@
+package com.example.redditclone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @NotBlank(message = "Post Name cannot be empty or null")
+    private String postName;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id",referencedColumnName = "id")
+    private Subreddit subreddit;
+}
